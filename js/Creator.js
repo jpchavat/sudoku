a1a9b78198ceb741cff3bf02252834faa8e417e9
jp <jp@jp-VNB132>
2014-01-10 19:04:53 +0000

    Borrado de funciones de armado de solución (se pasaron al php) Separación de solicitud de nuevo tablero en nueva función drawTable

--- js/Creator.js
+++ js/Creator.js
@@ -1,18 +1,19 @@
 	//i=mueve vertical, j=mueve horizontal
 
 	
-	
-	
 	function solveSudoku(){
 		var matrix = getMatrixfromHtml();
 		var posValsmatrix = getMatrixPosibilities(matrix);
 		
+		console.log('Tablero solucionado');
 		
-		//console.log(findNakedCell(posValsmatrix));
-		
-		
-		
-		
+	}
+  
+	function newSudoku(){
+    console.log('Nuevo tablero.');		
+    drawTable();
+    assignGrid(createGrid());
+  	
 	}
 	
 
@@ -101,15 +102,8 @@
 		return numList;
 	}
 
-
-
-
-	/***************************************************
-	***************** REGION TO CREATE *****************
-	*****************    THE MATRIX    *****************   
-	***************************************************/
-	function createTable(){
-		var table = $('<table cellspacing="0" cellpadding="0" ><tbody>');
+  function drawTable(){
+    var table = $('<table cellspacing="0" cellpadding="0" ><tbody>');
 		for(var i = 1; i < 10; i++){
 			var tr = $('<tr>');	
 			for (var j = 1; j < 10; j++){
@@ -138,14 +132,31 @@
 			}
 			tr.appendTo(table);
 		}
-		table.appendTo('#SudGame');
-		var butt = $('<button/>', {
-						text: 'solve',
-						id: 'btn',
+		$('#SudGame').html(table);
+    
+    tableValidations();
+  }
+
+
+	/***************************************************
+	***************** REGION TO CREATE *****************
+	*****************    THE MATRIX    *****************   
+	***************************************************/
+	function createTable(){
+		drawTable();
+		var buttNew = $('<button/>', {
+						text: 'New Game',
+						id: 'btnNew',
+						click: function () { newSudoku(); }
+					}); 
+		buttNew.appendTo('#SudControl');
+		var buttSolve = $('<button/>', {
+						text: 'Solve',
+						id: 'btnSolve',
 						click: function () { solveSudoku(); }
 					}); 
-		butt.appendTo('#SudGame');
-		tableValidations();
+		buttSolve.appendTo('#SudControl');
+		
 	}
 	
 	function tableValidations(){
@@ -171,58 +182,20 @@
 	}
 
 	function createGrid(){
-    var matrix;
-    
+    var matrix;    
     
     $.ajax({
-            type: 'POST',
-            url: 'src/getGrid.php',
-            format: 'JSON',
-            async: false
-          }).done(function(data){
-            matrix = data.usr;
-            //console.log(data);
-          });
-          
-          
-		/*var matrix = [
-						  [1,'',3,	4,'',6,	7,8,9],
-						  [4,5,6,	7,8,9,	1,2,3],
-						  [7,8,9,	1,2,3,	4,5,6], 
-						  
-						  [2,3,4,	5,6,7,	8,9,1],
-						  [5,'',7,	8,9,1,	2,3,4],
-						  [8,9,1,	2,3,4,	5,6,7],
-						  
-						  [3,4,5,	6,7,8,	9,1,2],
-						  [6,7,8,	9,1,2,	3,4,5],
-						  [9,1,2,	3,4,5,	6,7,8]
-					  ];*/
-		
-		var ramLow = 80; var ramHig = 140;
-		var ramLow = 0; var ramHig = 0;
-		var times = Math.floor(Math.random()*(ramHig-ramLow)) +ramLow;
-		for(var i=0;i<times;i++){
-			var auxRam = [0,1,2,3,4];
-			var aux = auxRam[Math.floor(Math.random()*(auxRam.length))];
-			switch (aux) {
-				case 0:
-					matrix = ramdomizeCols(matrix);
-				break;
-				case 1:
-					matrix = ramdomizeRows(matrix);
-				break;
-				case 2:
-					matrix = ramdomizeSqaCols(matrix);
-				break;
-				case 3:
-					matrix = ramdomizeSqaRows(matrix);
-				break;
-				case 4:
-					matrix - ramdomizeDigits(matrix);
-				break;
-			}
-		}
+          type: 'POST',
+          url: 'src/getGrid.php',
+          format: 'JSON',
+          async: false
+        }).done(function(data){
+          matrix = data.usr;
+          console.log('Llamado AJAX a nuevo tablero ok.');
+        }).fail(function(){
+          console.error('Error en llado AJAX a nuevo tablero.')
+        });
+	
 		return matrix;
 	}
 	/***************************************************
@@ -230,114 +203,6 @@
 	***************\\    THE MATRIX    *****************   
 	***************************************************/
 
-
-
-	/***************************************************
-	***************** REGION TO RAMDOM *****************
-	*****************    THE MATRIX    *****************   
-	***************************************************/	
-	function ramdomizeDigits(matrix){
-		var digit1 = Math.floor(Math.random()*(9))+1;
-		var digit2 = Math.floor(Math.random()*(9))+1;
-		digit1 = 1;
-		digit2 = 2;
-		var changed = true;
-		for(var i = 0; i < 9 ; i++){
-			for(var j = 0; j < 9 ; j++){
-				if(matrix[i][j] == digit1 ){
-					matrix[i][j] = digit2;
-					changed = false;
-				}
-				if(matrix[i][j] == digit2 && changed){
-					matrix[i][j] = digit1;
-				}
-				changed = true;
-			}
-		}
-		return matrix;
-	}
-
-	function ramdomizeSqaCols(matrix){
-		var cols = [0,1,2];
-		var col1 = cols[Math.floor(Math.random()*(cols.length))];
-		cols.remove(col1);
-		var col2 = cols[Math.floor(Math.random()*(cols.length))];
-		var colTemp1 = [];
-		var colTemp2 = [];
-		var colTemp3 = [];
-		for (var i2 = 0;i2<9;i2++){
-			colTemp1[i2] = matrix[i2][(col1*3)];
-			colTemp2[i2] = matrix[i2][(col1*3)+1];
-			colTemp3[i2] = matrix[i2][(col1*3)+2];
-			matrix[i2][(col1*3)]= matrix[i2][(col2*3)];
-			matrix[i2][(col1*3)+1]= matrix[i2][(col2*3)+1];
-			matrix[i2][(col1*3)+2]= matrix[i2][(col2*3)+2];
-			matrix[i2][(col2*3)] = colTemp1[i2];
-			matrix[i2][(col2*3)+1] = colTemp2[i2];
-			matrix[i2][(col2*3)+2] = colTemp3[i2];
-		}
-		return matrix;
-	}
-	
-	function ramdomizeSqaRows(matrix){
-		var rows = [0,1,2];
-		var row1 = rows[ Math.floor(Math.random()*(rows.length)) ];
-		rows.remove(row1);
-		var row2 = rows[ Math.floor(Math.random()*(rows.length)) ];
-		var rowTemp1 = [];
-		var rowTemp2 = [];
-		var rowTemp3 = [];
-		for(var j=0;j<9;j++){
-			rowTemp1[j] = matrix[(row1*3)][j];
-			rowTemp2[j] = matrix[(row1*3)+1][j];
-			rowTemp3[j] = matrix[(row1*3)+2][j];
-			matrix[(row1*3)][j] = matrix[(row2*3)][j];
-			matrix[(row1*3)+1][j] = matrix[(row2*3)+1][j];
-			matrix[(row1*3)+2][j] = matrix[(row2*3)+2][j];
-			matrix[(row2*3)][j] = rowTemp1[j];
-			matrix[(row2*3)+1][j] = rowTemp2[j];
-			matrix[(row2*3)+2][j] = rowTemp3[j];
-		}
-		return matrix;
-	}
-	
-	function ramdomizeCols(matrix){
-		var colSqa = Math.floor(Math.random()*(3)) ;
-		var cols = [0,1,2];
-		var col1 = cols[Math.floor(Math.random()*(cols.length))];
-		cols.remove(col1);
-		var col2 = cols[Math.floor(Math.random()*(cols.length))];
-		col1 = (colSqa*3) + col1;
-		col2 = (colSqa*3) + col2;
-		var colTemp = [];
-		for (var i2 = 0;i2<9;i2++){
-			colTemp[i2] = matrix[i2][col1];
-			matrix[i2][col1] = matrix[i2][col2];
-			matrix[i2][col2] = colTemp[i2];
-		}	
-		return matrix;
-	}
-	
-	function ramdomizeRows(matrix){
-		var rowSqa =  Math.floor(Math.random()*(3)) ;
-		var rows = [0,1,2];
-		var row1 = rows[Math.floor(Math.random()*(rows.length))];
-		rows.remove(row1);
-		var row2 = rows[Math.floor(Math.random()*(rows.length))];
-		row1 = (rowSqa*3) + row1;
-		row2 = (rowSqa*3) + row2;
-		var rowTemp = [];
-		for (var j = 0;j<9;j++){
-			rowTemp[j] = matrix[row1][j];
-			matrix[row1][j] = matrix[row2][j];
-			matrix[row2][j] = rowTemp[j];
-		}	
-		return matrix;
-	}
-	/***************************************************
-	***************\\ REGION TO RAMDOM *****************
-	***************\\    THE MATRIX    *****************   
-	***************************************************/	
 
 	Array.prototype.remove = function() {
 		var what, a = arguments, L = a.length, ax;
